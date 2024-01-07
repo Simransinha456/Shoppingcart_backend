0a6dca1f68ae9468f7b704a410fe762dd621bf35
Simran Sinha <simransinha280@gmail.com>
2024-01-07 23:06:18 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -14,20 +14,20 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Api Working");
 });
+const connect = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://simransinha:MERNpassword@recipes.xrnkcsc.mongodb.net/recipes?retryWrites=true&w=majority");
+    console.log("Connected To MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+connect();
 app.use("/auth", userRouter); 
 app.use("/recipes", recipesRouter);
 
 
 
-const connect = async () => {
-    try {
-      await mongoose.connect("mongodb+srv://simransinha:MERNpassword@recipes.xrnkcsc.mongodb.net/recipes?retryWrites=true&w=majority");
-      console.log("Connected To MongoDB.");
-    } catch (error) {
-      throw error;
-    }
-  };
-connect();
 
 app.listen(8000, ()=>{
     console.log("SERVER STARTED")
